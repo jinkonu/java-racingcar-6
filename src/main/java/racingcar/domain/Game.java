122482bffbd6a709797ab116cad8155363550e01
Jin Geonwoo <jinkonu99@gmail.com>
2023-11-27 20:36:03 +0900

    feat: Game의 run()에 게임 진행 로직 추가

--- src/main/java/racingcar/domain/Game.java
+++ src/main/java/racingcar/domain/Game.java
@@ -1,36 +1,58 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.utils.dto.WinnerCarsDto;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static racingcar.utils.constant.NumberConstant.*;
+import static racingcar.view.InputView.*;
+import static racingcar.view.OutputView.*;
 
 public class Game {
 
     private List<Car> cars;
+    private final int turn;
 
 
     public static void run() {
+        Game game = new Game();
 
+        game.tryFor(game.turn);
+
+        printResult();
+
+        WinnerCarsDto winnerCars = WinnerCarsDto.of(game.getWinners());
+        print(winnerCars);
     }
 
 
-    public Game(List<String> names) {
-        cars = new ArrayList<>();
+    private Game() {
+        printNameInputForm();
+        cars = readNames()
+                .stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
 
-        names.forEach(name -> cars.add(new Car(name)));
+        printTurnInputForm();
+        this.turn = readTurn();
     }
 
 
-    public void tryFor(int turn) {
-        cars.forEach(car -> car.goFor(Random.generate()));
+    private void tryFor(int turn) {
+        IntStream.range(ZERO, turn).forEach(i ->
+                cars.forEach(car -> {
+                    car.goFor(Random.generate());
+                    print(car);
+                })
+        );
     }
 
 
-    public List<Car> getWinners() {
+    private List<Car> getWinners() {
         Collections.sort(cars);
 
         return cars.subList(FIRST_IDX, cars.lastIndexOf(getLast()));
